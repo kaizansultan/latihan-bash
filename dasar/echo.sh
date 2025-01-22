63aee074d545b6bd026d7d109b25a0d7be04e3bb
kaizansultan <kaizansultan@proton.me>
2025-01-22 23:27:16 +0700

    feat: add basic syntax tutors

--- dasar/echo.sh
+++ dasar/echo.sh
@@ -1,1 +1,8 @@
+#! /bin/bash
+
+# di atas ini adalah shebang `#!`, bukan komentar. Di baris pertama ini kita
+# menentukan interpreter yang akan kita gunakan. Dalam kasus ini ialah bash:
+
+which bash
+
 echo "Halo Guys"
---
+++ dasar/execute.sh
@@ -0,0 +1,5 @@
+# Agar script bisa dieksekusi, gunakan perintah
+chmod +x echo.sh
+
+# Kemudian jalankan mengguanakan perintah seperti:
+./echo.sh
